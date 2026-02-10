2370b6fe93dca0cb8b1d995eadce72cdae0847f2
Sakshi Manik Sherkhane <sakshisherkhane2006@gmail.com>
2026-02-10 09:32:11 +0530

    Sorting Part2

---
+++ ExperimentNo5/MergeSort.java
@@ -0,0 +1,69 @@
+package ExperimentNo5;
+
+class MergeSort {
+
+    void merge(int arr[], int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void sort(int arr[], int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {6, 3, 8, 2, 5};
+        int n = arr.length;
+
+        MergeSort ms = new MergeSort();
+        ms.sort(arr, 0, n - 1);
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
