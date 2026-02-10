2455be1c82787f9bc90a5f7136f448a5efc9ece2
Sakshi Manik Sherkhane <sakshisherkhane2006@gmail.com>
2026-02-10 09:27:56 +0530

    Sorting part2

---
+++ ExperimentNo5/InsertionSort.java
@@ -0,0 +1,27 @@
+package ExperimentNo5;
+
+class InsertionSort {
+    public static void main(String[] args) {
+
+        int arr[] = {5, 0, 64, -1, 33};
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
